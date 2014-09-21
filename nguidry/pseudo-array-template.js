d08885069a410db3f97d923ed7ab96901ba3aaa3
nguidry <nguidry7@gmail.com>
2014-09-21 15:36:01 -0700

    exercise 1 complete

--- nguidry/pseudo-array-template.js
+++ nguidry/pseudo-array-template.js
@@ -9,6 +9,7 @@
 //_Hint:_ Within each method, to refer to your array object, you may use either the global 
 // variable _array_ or the keyword _this_.
 
+
 var array = {length:0};
 
 array.pop = function() {
@@ -16,26 +17,23 @@ array.pop = function() {
   return --this.length;
 }
 
-var newValue="";
-
 array.push = function(newValue) { 
+	var newValue = "";
 	this[this.length] = newValue;
 	return ++this.length;
 }
 
-//var array = {length:0, 1:"jimbo", 2:"oph", 3:"mona"};
-
 array.join = function(joinString) { //returns a string
-	var arrayString = "";
-	for (var key in array) {
-    arrayString = arrayString + this[key] + joinString;
-    //need to account for cutoff before final element (works otherwise)
-  }
-  
+	var arrayString = "";	
+	for (var propNum = 0; propNum < this.length; propNum++) {
+		if (propNum < (this.length-1)) {	
+    		arrayString = arrayString + this[propNum] + joinString;
+    	} else {
+    		arrayString = arrayString + this[propNum];
+    	}
+	}
 	return arrayString;
 }
-
-//array.join(' + ');
 
 //**b)**  Assuming you've implemented _array_ correctly, predict the outcome
 //of the following:
@@ -47,9 +45,11 @@ array.push('a');
 array.pop();
 console.log(array.join('a')); //What will this print?
 
+// this prints the string "cab"
+
 //**c)**  Similarly, predict the outcome here:
 
 array.length=0;
 console.log(array.join(array.push(array.push('a')))); // What will this print?
 
-
+// this prints the string "a21"
