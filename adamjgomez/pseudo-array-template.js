9efe02ec067467828348875249b5828de0b6d8af
Adam Gomez <adamjgomez@gmail.com>
2014-09-21 16:32:25 -0700

    updated pseudo-array answers. Added cards2 file to adamjgomez directory.

--- adamjgomez/pseudo-array-template.js
+++ adamjgomez/pseudo-array-template.js
@@ -1,16 +1,26 @@
 var array = {length:0};
 
-array.pop = function() { //changes array, returns ??
-  //...
+array.push = function(value) {
+  this[this.length] = value
+  return ++this.length;
 }
 
-array.push = function(/*...*/) { //changes array, returns a number
-  //...
+array.pop = function() {
+  delete this[this.length-1];
+  return --this.length;
 }
 
-array.join = function(/*...*/) { //returns a string
-  //...
+array.join = function(separator) { 
+  if (separator === undefined)
+    separator = ', ';
+  var stringify = '';
+  for (var i=0; i<this.length; i++) {
+    if (this[i]!==undefined)
+      stringify = stringify + this[i];
+    if (i < this.length - 1)
+      stringify = stringify + separator;
+  }
+  return stringify;
 }
-
 
 
