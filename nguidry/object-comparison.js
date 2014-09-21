34f654dae30397357a0a91b500f4982a6c5b114c
nguidry <nguidry7@gmail.com>
2014-09-21 11:20:47 -0700

    laid out some pseudocode for exercise 3

---
+++ nguidry/object-comparison.js
@@ -0,0 +1,16 @@
+// var obj = {color:"red", shape:"circle", tea:"green"};
+// var copiedObj = {};
+
+function copy(obj) {
+	//create new object
+	var copiedObj = {};
+	//use for in loop to go through input obj
+	for (var prop in obj) {
+		//create var to copy input prop to output prop
+		//create var to copt input prop value to output prop val
+		//copy each property and value of input object 
+		//to the output object
+
+	}
+}
+
