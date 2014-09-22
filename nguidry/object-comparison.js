c943b7e6b0e0bdb14439b70ae430bed6794525e4
nguidry <nguidry7@gmail.com>
2014-09-21 17:55:14 -0700

    progress on exercise 3

--- nguidry/object-comparison.js
+++ nguidry/object-comparison.js
@@ -1,41 +1,70 @@
-// var obj = {color:"red", shape:"circle", tea:"green"};
-// var copiedObj = {};
-
-
 // *** a ***
-function copy(obj) {
-	//create new object
-	var copiedObj = {};
-	//use for in loop to go through input obj
-	for (var prop in obj) {
-		//create var to copy input prop to output prop
-		//create var to copt input prop value to output prop val
-		//copy each property and value of input object 
-		//to the output object
 
+function copy(obj) {
+	for (var prop in obj) {
+		copiedObj[prop] = obj[prop];
 	}
 }
 
 function equal(objA, objB) {
-	//return true only when `objA` and `objB` have 
-	//exactly the same properties with the same values.  
-	//Note that two empty objects should be considered equal.
+	for (var prop in objA) {
+		if (!(prop in objB)) {return false}; 
+		if (objA[prop] !== objB[prop]) {return false};
+	}
+	
+	for (var prop in objB) {
+		if (!(prop in objA)) {return false}; 
+		if (objB[prop] !== objA[prop]) {return false};
+	}
+	
+	return true;
 }
 
 function similar (objA, objB) {
-	//should return true only when `objA` and `objB` have 
-	//exactly the same properties, regardless of their values
+	for (var prop in objA) {
+		if (!(prop in objB)) {return false}; 
+	}
+	
+	for (var prop in objB) {
+		if (!(prop in objA)) {return false}; 
+	}
+	
+	return true;
 }
 
 // *** b ***
 
 function union(objA,objB) {
+	// evaluate whether properties are the same in each
+	for (var prop in objA) {
+		unitedObj[prop] = objA[prop];
+	}
 
+	for (var prop in objB) {
+		if (!(prop in unitedObj)) {
+			unitedObj[prop] = objB[prop];
+		}
+		if (objB[prop] !== objA[prop]) {
+			unitedObj[prop] = (objA[prop] || objB[prop]);
+		}
+	}
 }
+
+// The intersection of objects A,B is a new object which contains only those properties found in BOTH A and B.  
+// The value of each intersecting property is `(A[key] && B[key])`.
+// For example, the intersection of `{a:1,b:0}` and `{a:0,c:0}` is `{a:0}`.
+
+var objA = {1: "test", 2:"pencil", k:true};
+var objB = {2: "frog", k: true, z:"llama"};
+var intObj = {};
 
 function intersect(objA,objB) {
 
 }
+
+// The subtraction of B from A, aka "A minus B", is an object which contains all the properties of A which are NOT in B.  
+// Note that this merge is usually not symmetric: _A minus B_ doesn't equal _B minus A_ (except in one case, which you 
+// should identify!) For example, `{a:1,b:0}` minus `{a:0,c:0}` is `{b:0}`, and the reverse subtraction is `{c:0}`.
 
 function subtract(objA,objB) {
 
