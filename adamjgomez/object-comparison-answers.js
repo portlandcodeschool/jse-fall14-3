f4c6d35031b14e42e9e721cbff237688f22ab16c
Adam Gomez <adamjgomez@gmail.com>
2014-09-23 08:56:37 -0700

    Updated pseudo array code and object comparison code.

---
+++ adamjgomez/object-comparison-answers.js
@@ -0,0 +1,100 @@
+// a) Write a function copy(obj), which duplicates an object (not just copying a reference to it). You only need to duplicate only one level: if obj contains another object inner, the duplicate may share inner rather than copying it too.
+
+var obj = { first: 'a', second: 'b', third: 'c'}
+
+function copy(obj) {
+
+  var dup = {};
+  for (var key in obj) {
+    dup[key]=obj[key];
+  }
+  return dup;
+}
+
+
+
+
+
+// Write another function to compare two objects: equal(objA,objB) should return true only when objA and objB have exactly the same properties with the same values. Note that two empty objects should be considered equal.
+
+
+var obj1 = { a:'apple', b:'bean', c:'carrot'}
+
+var obj2 = { a:'apple', b:'bean', c:'pie'}
+
+var compare = function(obj1, obj2) {
+  for (var key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// Write a third function: similar(objA,objB) should return true only when objA and objB have exactly the same properties, regardless of their values.
+
+
+var objA = { a:'fall', b:'spring', c:'summer'};
+
+var objB = { a:'apple', b:'bean', d:'pie'};
+
+var similar = function(objA, objB) {
+  for (var key in objA) {
+    if (key in objB !== key in objA) { // a better way to write it is: if (!(key in objB));
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+meet (arg2) = people.index[arg1]
+
+
+
+// object for joe
+// and an object for joe's friends list.
+
+
+// people.index[who].friends
+
+
+
+
+//set to zero
+// people.index[who].friends[whichFriend]
+
+
+
+// meet function should go both ways.
+
+//
+
+who = Dan
+whoElse = Adam
+
+people.index[who].friends[whichFriend]
+people.index[whoElse].friends[who]
+
+
+increment 
+//make sure that these boxes are build for a new person so you can go down that path with out creating an error.
+
+
--- adamjgomez/pseudo-array-template.js
+++ adamjgomez/pseudo-array-template.js
@@ -10,17 +10,46 @@ array.pop = function() {
   return --this.length;
 }
 
-array.join = function(separator) { 
-  if (separator === undefined)
-    separator = ', ';
-  var stringify = '';
-  for (var i=0; i<this.length; i++) {
-    if (this[i]!==undefined)
-      stringify = stringify + this[i];
-    if (i < this.length - 1)
-      stringify = stringify + separator;
+array.join = function(separator) { // .join is a method
+  if (separator === undefined) {   // Determin if separator is undefined.
+    separator = ', '; // If separator is undefined do this.
+  }
+  var stringify = ''; // Create separator variable and set it equal to an empty string.
+  for (var count=0; count<this.length; count++) { // Set up counter starting at zero and incrementor
+    if (count < this.length - 1) {
+      stringify += this[count] + separator;
+    } else {
+      stringify += this[count];
+    }
   }
   return stringify;
 }
+
+
+
+
+
+// Notes from MDN
+
+/*
+Strict equality and undefined
+
+You can use undefined and the strict equality and inequality operators to determine whether a variable has a value. In the following code, the variable x is not defined, and the if statement evaluates to true.
+
+var x;
+if (x === undefined) {
+   // these statements execute
+   console.log("this is undefined dude")
+}
+else {
+   // these statements do not execute
+   console.log("All good.")
+}
+
+*/
+
+
+
+
 
 
