15ca5961124b691ec49cb36a9a97a077bd0ef12e
Dallasss7 <mr.dalsummers@gmail.com>
2014-09-24 17:56:38 -0700

    final push for social network object

--- social-network-template.js
+++ social-network-template.js
@@ -1,18 +1,99 @@
 
-var people = {timesMet:0};
+var people = {};
 
 people.index = {};
 
 people.meet = function(nameA,nameB) {
 
+	var person1 = nameA.toLowerCase();
+
+	var person2 = nameB.toLowerCase();
+	
+	if(person1 != person2){
+
+		if(!([nameA] in people.index)){
+			
+			people.index[nameA] = {name:nameA, friends:{}};
+		
+		}
+
+		if(!([nameB] in people.index)){
+
+			people.index[nameB]= {name:nameB, friends:{}};
+
+		}
+
+		if(!([nameB] in people.index[nameA].friends)){
+
+			people.index[nameA].friends[nameB] = 1;
+
+		} else{
+			
+			++people.index[nameA].friends[nameB];
+			
+			}
+
+		if(!([nameA] in people.index[nameB].friends)){
+
+			people.index[nameB].friends[nameA] = 1;
+		
+		} else{
+			
+			++people.index[nameB].friends[nameA];
+			
+			}
+	
+	} else{
+
+		return 'Names are equal. You have already met yourself.';
+	};
+
 }
 
 people.haveMet = function(nameA,nameB) { 
 
+	if([nameA] != [nameB]){
+	
+		if(!([nameB] in people.index[nameA].friends)){
+		
+			return 'These two have not met yet';
+		
+		}else{
 
+		return people.index[nameA].friends[nameB];
+
+		}
+	
+	}else{
+		
+		return 'Yes, you have met yourself already';
+	
+	}
 }
 
-people.friendsOf = function(name) { //returns a string
-	//...
+people.friendsOf = function(name) { 
+	
+	var arr = [];
+	
+	if(!([name] in people.index)){
+			return 'name not found in index';
+		}
+	
+	else{	
+
+		for(var key in people.index[name].friends){
+			arr.push(key);
+		}
+	}
+	
+	return arr.sort();
 }
+
+
+
+
+
+
+
+
 
